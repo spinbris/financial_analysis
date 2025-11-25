8e7939ebf0f6e11d1d534df273823f2e1969266a
spinbris <stephen.parton@gmail.com>
2025-11-25 10:11:39 +1000

    fix: move FinancialDataCache into cache package for Railway

--- financial_research_agent/cache/__init__.py
+++ financial_research_agent/cache/__init__.py
@@ -1,6 +1,6 @@
 """Financial data caching module."""
-
 from .sec_financial_cache import SecFinancialCache
 from .cached_manager import CachedFinancialManager
+from .data_cache import FinancialDataCache
 
-__all__ = ['SecFinancialCache', 'CachedFinancialManager']
+__all__ = ['SecFinancialCache', 'CachedFinancialManager', 'FinancialDataCache']
